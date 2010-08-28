3832755e3766fa01f3a321772a9bfc96e71925be
Huned Botee <hunedx@gmail.com>
2010-08-27 18:23:09 -0700

    server with socket.io stuff stubbed out

--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('<3 O HAI NKO <3!\n');
-}).listen(80);
-console.log('server running');
+var io = require('socket.io.js');
+
+//
+// http
+//
+
+server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Hello world</h1>');
+  res.close();
+});
+
+server.listen(8080);
+
+//
+// websocket
+//
+
+var socket = io.listen(server);
+
+socket.on('connection', function(client) {
+  client.on('message', function() {
+    console.log(".");
+  });
+
+  client.on('disconnect', function() {
+    console.log("disconnect");
+  });
+});
+
+console.log('running');
